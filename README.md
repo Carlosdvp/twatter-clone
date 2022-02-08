77975082a19f8cdfb183873c1d97db1a19a13900
Carlos V <carlosdvp.tx@gmail.com>
2022-02-08 14:29:34 -0400

    added a new component for the UserProfile and a follower counter button.

--- README.md
+++ README.md
@@ -17,3 +17,35 @@ npm run build
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
+---------------------------------------------------------------------------
+
+# Project Details
+
+- Using vue-cli for the setup and build steps
+- basic project
+	- Intermediate level
+
+
+## Goals
+
+* Get a clear understanding of Vuex and how to implement in a somewhat complex application
+* Review the basics of Vue 3 and fil in the gaps in understanding for basic project creation steps.
+
+
+## Part 1
+
+### Building Single File Components
+	- Understand the basics of using:
+		- the data() property
+		- `watch` different data points inside the component and do something when the change happens
+		- `computed properties`
+		- `methods`
+		- lifecycle hooks, here we saw `mounted()` to start
+
+
+## Part 2
+
+### Template Conditionals, loops and events
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,60 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Santa Maria" src="../assets/logo.png">
+    <p>{{ user.username }} -- {{ fullname }}</p>
+    <p>Followers: <strong>{{ followers }}</strong></p>
+    <button @click="followUser">Follow</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+  },
+  data() {
+    return {
+      followers: 0,
+      // placeholder usr object
+      user: {
+        id: 1,
+        username: 'Queen of Heaven',
+        firstName: 'Santa',
+        lastName: 'Maria',
+        email: 'Gunner@bullets.com',
+        isAdmin: true
+      }
+    }
+  },
+  watch: {
+    // watches a method or computed property
+    // everytime the item watched changes we do something
+    followers(newFollowerCount, oldFollowerCount) {
+      if (oldFollowerCount < newFollowerCount) {
+        console.log(`${this.user.username} has gained a new follower.`)
+      }
+    }
+  },
+  computed: {
+    // first computed property
+    fullname() {
+      return `${this.user.firstName} ${this.user.lastName}`
+    }
+  },
+  methods: {
+    // first method
+    followUser() {
+      this.followers++
+    }
+  },
+  // lifecycle hooks -- mounted runs when the compoenet is first loaded
+  // API calls can be implemented here
+  mounted() {
+    this.followUser() // function is run the moment the component is loaded
   }
+
 }
 </script>
